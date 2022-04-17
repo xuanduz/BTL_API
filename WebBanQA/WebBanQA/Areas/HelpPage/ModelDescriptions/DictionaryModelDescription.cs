fa099c5172fabe1fe7eac3edb5450feb6e499c4c
xuanduz <xuanduc942001@gmail.com>
2022-04-17 21:58:22 +0700

    create project

---
+++ WebBanQA/WebBanQA/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebBanQA.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebBanQA/WebBanQA/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Cors;
+using System.Web.Mvc;
+
+namespace WebBanQA.Controllers
+{
+    // khi them moi controller thi phai go cai dong nay
+    [EnableCors("*", "*", "*")]
+
+    public class HomeController : Controller
+    {
+        
+    }
+}
