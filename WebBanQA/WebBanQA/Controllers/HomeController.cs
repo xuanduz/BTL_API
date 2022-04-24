fc017b832c344e68e824e79c041c98c33f32ccae
xuanduz <xuanduc942001@gmail.com>
2022-04-24 17:25:34 +0700

    backup project

--- WebBanQA/WebBanQA/Controllers/HomeController.cs
+++ WebBanQA/WebBanQA/Controllers/HomeController.cs
@@ -37,5 +37,47 @@ namespace WebBanQA.Controllers
             return true;
         }
         
+        [HttpPost]
+        //[Route("api/register/{fname}/{lname}/{uname}/{password}/{email}/{phonenumber}/{address}")]
+        [Route("api/register")]
+        public string Register(string fname, string lname, string uname, string password, string email, string phonenumber, string address)
+        {
+            try
+            {
+                DBUserDataContext db = new DBUserDataContext();
+                User user = new User();
+                var checkUserName = db.Users.Where(x => x.U_name == uname).SingleOrDefault();
+                var checkEmail = db.Users.Where(x => x.U_email == email).SingleOrDefault();
+
+                if(checkUserName != null)
+                {
+                    return "Username exist";
+                }
+                else if(checkEmail != null)
+                    {
+                        return "Email exist";
+                    }
+                    else
+                    {
+                        user.U_id = "";
+                        user.U_Fname = fname;
+                        user.U_Lname = lname;
+                        user.U_name = uname;
+                        user.U_email = email;
+                        user.U_status = "true";
+                        user.U_add = address;
+                        user.U_contact = phonenumber;
+                        user.U_created = DateTime.Now;
+                        user.U_pass = password;
+                        db.Users.InsertOnSubmit(user);
+                        db.SubmitChanges();
+                        return "Success";
+                    }
+            }
+            catch
+            {
+                return "Fail";
+            }
+        } 
     }
 }
